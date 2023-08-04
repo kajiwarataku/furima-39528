a57b0ca5329285fd9ae9c2238d6cd9a6689e18b2
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-04 11:01:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
 end
