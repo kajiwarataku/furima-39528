25d4030fb676baf64bc6b06976efc42cbd323943
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-08 10:09:35 +0900

    カテゴリーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,26 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.includes(:user)
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :condition_id, :bear_id, :area_id, :send_day_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,19 @@
 class Item < ApplicationRecord
   belongs_to :user
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :bear
+  belongs_to :area
+  belongs_to :send_day
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :bear_id, presence: true
-  validates :area_id, presence: true
-  validates :send_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :bear_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :send_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
   validates :user, presence: true
 end
