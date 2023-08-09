da09f0fc9bc6ebb0f7c888fe662f4ef3812efce6
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-09 14:58:53 +0900

    単体テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
+
   factory :item do
-    
+    name               { '車' }
+    description        { '大切に乗っていました' }
+    category_id        { 2 }
+    condition_id       { 2 }
+    bear_id            { 2 }
+    area_id            { 2 }
+    send_day_id        { 2 }
+    price              {1234567}
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/sample/sky.jpeg'), filename: 'sky.jpeg')
+    end
   end
 end
