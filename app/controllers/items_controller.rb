2e89594632bb70bfcf9abbd1556bc4b7384875f7
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-10 22:47:30 +0900

    変更内容が反映されるように

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,7 +32,16 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
