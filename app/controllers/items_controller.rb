1413280035bd0f20a303a3db1f40b27f70c9b006
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-11 14:15:58 +0900

    コード修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show, :new, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -20,7 +20,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     return unless user_signed_in?
 
     @is_owner = current_user == @item.user
@@ -28,14 +27,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     return unless !user_signed_in? || current_user != @item.user
 
     redirect_to root_path
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
