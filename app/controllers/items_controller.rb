0b1f16779994e41dd0a28ddc946d976c44d8f3a4
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-11 11:28:02 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
