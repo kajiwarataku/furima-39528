25366170c7c1f6ad654303b57c98502aadea54d0
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-14 23:53:45 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items do
+    resources :purchases, only: [:index]
+   end
 end
