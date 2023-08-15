e377f9851cb621250332a7d4bb347c7acb58d1db
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-15 23:48:16 +0900

    サーバーサイドで環境変数を読み込めるように

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -23,7 +23,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = "sk_test_58bbe72fec6a62c883c28813"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
