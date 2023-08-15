86a9ec94acb372099dd275eb31b6d19b7edac8da
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-15 17:48:36 +0900

    コントローラー記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,29 @@
 class PurchasesController < ApplicationController
+  before_action :non_purchased_item, only: [:index, :create]
 
   def index
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(purchase_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:post_code , :area_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def non_purchased_item
     @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
   end
 end
