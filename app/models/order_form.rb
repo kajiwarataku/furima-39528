997a90a25e660b23d5f630dda7670a93e74cf131
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-15 17:33:19 +0900

    Formモデル作成、記述

---
+++ app/models/order_form.rb
@@ -0,0 +1,20 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :area_id, :municipality, :house_number, :building_name, :telephone_number, :token
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :municipality
+    validates :house_number
+    validates :telephone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :token
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    Payment.create(purchase_id: purchase.id, post_code: post_code, area_id: area_id, municipality: municipality, house_number: house_number, building_name: building_name, telephone_number: telephone_number)
+  end
+end
