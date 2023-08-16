f354a067339df5a77ada10d9692e1256d6075404
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-16 19:01:05 +0900

    商品購入機能の単体テストコード

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,16 +2,15 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :area_id, :municipality, :house_number, :building_name, :telephone_number, :token
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :municipality
-    validates :house_number
-    validates :telephone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-    validates :token
-  end
+    validates :user_id, presence: true
+    validates :item_id, presence: true
+    validates :post_code, presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :area_id, presence: true, numericality: { other_than: 0, message: "can't be blank" }
+    validates :municipality, presence: true
+    validates :house_number, presence: true
+    validates :telephone_number, presence: true, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :token, presence: true
+
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
