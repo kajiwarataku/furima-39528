5cfc4b838718a40b1be4230943dcc2383c3cf252
kajiwarataku <138559831+kajiwarataku@users.noreply.github.com>
2023-08-17 22:10:55 +0900

    ログアウトの調整

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   resources :items do
     resources :purchases, only: [:index, :create]
    end
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
